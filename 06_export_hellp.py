099a7c3060a5b0a0255c82f67cb871fe356a8deb
Woojin Jeon <jeonw70450@masseyhigh.school.nz>
2021-02-23 10:49:08 +1300

    Changed the file names and added .sort()

--- 06_export_hellp.py
+++ 06_export_hellp.py
@@ -1,24 +1,26 @@
 import os
 
 copper = []
+
 for cu in os.listdir("Mystery_box_winnings_images/cu"):
     if cu.endswith(".gif"):
-        copper.append(cu)
+        copper.append("Mystery_box_winnings_images/cu/{}".format(cu))
 
+copper.sort()
 print(copper)
-
 
 silver = []
 for ag in os.listdir("Mystery_box_winnings_images/ag"):
     if ag.endswith(".gif"):
-        silver.append(ag)
+        silver.append("Mystery_box_winnings_images/ag/{}".format(ag))
 
+silver.sort()
 print(silver)
 
 gold = []
 for au in os.listdir("Mystery_box_winnings_images/au"):
     if au.endswith(".gif"):
-        gold.append(au)
+        gold.append("Mystery_box_winnings_images/au/{}".format(au))
 
+gold.sort()
 print(gold)
-
--- AA_Complied_game_v2.py
+++ AA_Complied_game_v2.py
@@ -240,21 +240,22 @@ class Game:
         stats_prizes = []
 
         copper = []
-
         for cu in os.listdir("Mystery_box_winnings_images/cu"):
             if cu.endswith(".gif"):
                 copper.append("Mystery_box_winnings_images/cu/{}".format(cu))
-
+        copper.sort()
 
         silver = []
         for ag in os.listdir("Mystery_box_winnings_images/ag"):
             if ag.endswith(".gif"):
                 silver.append("Mystery_box_winnings_images/ag/{}".format(ag))
+        silver.sort()
 
         gold = []
         for au in os.listdir("Mystery_box_winnings_images/au"):
             if au.endswith(".gif"):
                 gold.append("Mystery_box_winnings_images/au/{}".format(au))
+        gold.sort()
 
         for thing in range(0, 3):
 
